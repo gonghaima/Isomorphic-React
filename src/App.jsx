cbb054db8e981753ea4ab91dd6b776ee3fd629b7
steven <gonghaima@hotmail.com>
2018-03-23 15:03:41 +1100

    Building an Isomorphic React Application Part II – Creating React Functionality : Adding React

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const AppDisplay = () => {
+    return <div>
+        <h1>
+            Ismorphic React
+    </h1>
+    </div>
+};
+
+export default AppDisplay;
--- src/index.jsx
+++ src/index.jsx
@@ -1,1 +1,10 @@
-console.info("React is cool!");
+import App from './App.jsx';
+
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+const render = (_App) => {
+    ReactDOM.render(<_App />, document.getElementById('AppContainer'))
+};
+
+render(App);
